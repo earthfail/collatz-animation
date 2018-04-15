e7217edf473f17265734626cc4763687ec921f84
earthfail <saleemkhatib22@gmail.com>
2018-04-15 18:41:29 +0300

    working on 3n+1

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/client-pro "0.8.14"]])
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.6.0"]
+                 [com.h2database/h2 "1.4.193"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
--- src/expriments/core.clj
+++ src/expriments/core.clj
@@ -1,17 +1,47 @@
-(ns expriments.core)
+(ns expriments.core
+  (:require [clojure.math.numeric-tower :as mth]))
 
-;; (import 
-;;   '(java.awt Rectangle Dimension Robot Toolkit)
-;;   '(java.awt.image BufferedImage)
-;;   '(java.io File IOException)
-;;   '(javax.imageio ImageIO))
 
-;; (defn take-screenshot []
-;;   (let [screen (.getScreenSize (Toolkit/getDefaultToolkit)) 
-;;         rt (new Robot)
-;;         img (.createScreenCapture rt (new Rectangle (int (.getWidth screen)) (int (.getHeight screen))))]
-;;     (ImageIO/write img "jpg" (new File (str (System/currentTimeMillis) ".jpg")))))
+;;----------------NEW SHIP------------------------------------
+(import 
+  '(java.awt Rectangle Dimension Robot Toolkit)
+  '(java.awt.image BufferedImage)
+  '(java.io File IOException)
+  '(javax.imageio ImageIO))
+;; note never use M-q to organize code
+(defn take-screenshot [] (let [screen (.getScreenSize
+  (Toolkit/getDefaultToolkit)) rt (new Robot) img
+  (.createScreenCapture rt (new Rectangle (int (.getWidth screen))
+  (int (.getHeight screen))))] (ImageIO/write img "jpg" (new File (str
+  "resources/pics/" (System/currentTimeMillis) ".jpg")))))
 
 ;; (take-screenshot)
+;;----------------NEW SHIP------------------------------------
+(defn tail-fact
+  ([n] (tail-fact n 1))
+  ([n accum]
+   (if (zero? n)
+     accum
+     (tail-fact (dec n) (*' n accum)))))
 
-(defn always1 [x] 1)
+(defn e-to-n [n]
+  (reduce +'
+          (map #(/ (mth/expt Math/E %)) (range 1 n))))
+;;----------------NEW SHIP------------------------------------
+(defn collatz-basic [n]
+  (if (< 0 n)
+    (if (odd? n)
+      (inc (* 3 n))
+      (if (even? n)
+        (/ n 2)))))
+(defn format-line [n]
+  "write n in decimal, binary, octal and hex and goes to a new line"
+  (cl-format true "decimal ~d  binary ~b  octal ~o  hex ~x\n" n n n n))
+(doseq [l (take 20 (iterate #(map collatz-basic %) (range 1 10)))]
+  (println l))
+(defn count-ones [n]
+  (count (filter
+          {"1"}
+          (clojure.string/split
+           (cl-format nil "~b" n)
+           #"d"))))
