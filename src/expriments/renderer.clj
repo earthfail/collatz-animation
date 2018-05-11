1f666905787f5a78ec1f96d54503a8db0ea345c6
earthfail <saleemkhatib22@gmail.com>
2018-05-11 18:03:17 +0300

    make circles

--- src/expriments/renderer.clj
+++ src/expriments/renderer.clj
@@ -5,14 +5,21 @@
 (defn setup []
   (q/smooth)
   (q/frame-rate 1)
-  (q/background 0)
-  (q/text "Comic Sans" 10 10))
+  (q/background 255)
+  (q/no-fill)
+  (q/text "Shit is going Down¡" 10 10)
+  (def circle-group {1 [1 2 4 8 16 32]
+                     2 [3 5 6 10 9 18 36]
+                     3 [7 11 13 14]}))
 
 (defn draw []
-  (q/background 50)
-  (q/text "Comic Sans" 10 10)
+  (q/background 255)
   (q/translate (/ (q/width) 2) (/ (q/height) 2))
-  (q/ellipse 0 0 20 20)
-  (q/ellipse 50 50 30 30)
-  (q/rect 0 100 25 25)
-  (q/text "Khalti firnas" -100 30))
+  (doseq [[radius elements] circle-group]
+    (q/ellipse 0 0 (* 50 radius) (* 50 radius))
+    (let [ang (/ q/TWO-PI (count elements))]
+      (doall (map-indexed (fn [frac n]
+                            (q/text (str n)
+                                    (* radius (q/cos (* ang frac)))
+                                    (* radius (q/sin (* ang frac)))))
+                          elements)))))
